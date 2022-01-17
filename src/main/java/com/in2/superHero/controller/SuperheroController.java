8fff9b7ab955c5a412fb2ed7b09f4e2b761096b4
jekahwati <jekahwati@falabella.cl>
2022-01-17 00:32:55 -0300

    LogExecutionAnnotation is added

--- src/main/java/com/in2/superHero/controller/SuperheroController.java
+++ src/main/java/com/in2/superHero/controller/SuperheroController.java
@@ -1,5 +1,6 @@
 package com.in2.superHero.controller;
 
+import com.in2.superHero.annotations.logExecutionTime.LogExecutionTime;
 import com.in2.superHero.model.SuperHero;
 import com.in2.superHero.repository.SuperheroRepository;
 import com.in2.superHero.service.SuperheroService;
@@ -20,26 +21,31 @@ public class SuperheroController {
     SuperheroService superheroService;
 
     @PostMapping
+    @LogExecutionTime
     public ResponseEntity<SuperHero> addSuperHero(@RequestBody SuperHero superHero) {
         return superheroService.addSuperhero(superHero);
     }
 
     @GetMapping
+    @LogExecutionTime
     public List<SuperHero> getAllSuperHeros(@RequestParam(required = false) String name) {
         return superheroService.getAllSuperheros(name);
     }
 
     @GetMapping("/{id}")
+    @LogExecutionTime
     public Optional<SuperHero> findAllByNameContaining(@PathVariable Long id) {
         return superheroService.findById(id);
     }
 
     @PutMapping
+    @LogExecutionTime
     public ResponseEntity<SuperHero> updateSuperhero(@RequestBody SuperHero superHero) {
         return superheroService.update(superHero);
     }
 
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @LogExecutionTime
     public ResponseEntity<String> deleteSuperhero(@PathVariable Long id) {
         return superheroService.deleteSuperhero(id);
     }
